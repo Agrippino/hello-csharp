e7543147e1fddc47cbb7b0bdd5979196e39485c9
Agrippino <agrippinorussiello@outlook.it>
2022-03-31 15:29:07 +0200

    Add project files.

---
+++ HelloCsharp/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ciao, benvenuto in questo programma!");
+
+Console.WriteLine("Quale mese dell'anno preferisci?");
+Console.WriteLine("Gennaio");
+Console.WriteLine("Febbraio");
+Console.WriteLine("Marzo");
+Console.WriteLine("Arpile");
+Console.WriteLine("Maggio");
+Console.WriteLine("Giugnoo");
+Console.WriteLine("Luglio");
+Console.WriteLine("Agosto");
+Console.WriteLine("Settembre");
+Console.WriteLine("Ottobre");
+Console.WriteLine("Novembre");
+Console.WriteLine("Dicembre");
+
+Console.WriteLine("scrivi qui il mese che preferisci?");
+var testo = Console.ReadLine();
+Console.WriteLine(testo);
+
+Console.WriteLine(" bella scelta! Se cambi idea e vuoi dirmelo io sono qui,ciao!");
+
+
